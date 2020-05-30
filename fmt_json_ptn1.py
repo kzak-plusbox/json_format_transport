c5b5c17591ff45c626bb84347cf88cd6177d8aa1
kzak-plusbox <46486815+kzak-plusbox@users.noreply.github.com>
2020-05-30 22:45:35 +0900

    パターン1 実装いったん完了。動作確認実施済み

--- fmt_json_ptn1.py
+++ fmt_json_ptn1.py
@@ -1,3 +1,4 @@
+import os
 import glob
 import json
 import datetime
@@ -13,13 +14,24 @@ def create_csv(dict_datas):
         record_create_ts = (datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S')
         file_create_dt = (datetime.datetime.now()).strftime('%Y%m%d')
 
-        csv_list = ['店名,購入日時,作品名,作者,金額,購入数,レコード作成日時\n',]
+        flg = False
+        csv_list = []
+        if not os.path.exists('.\\files\\z_output_csv\\shopping_data_{0}.csv'.format(file_create_dt)):
+            # ファイルなければヘッダ追加
+            flg = True
+            csv_list.append('店名,購入日時,作品名,作者,金額,購入数,レコード作成日時')
+
         for data in dict_datas:
-            csv_list.append('通販本屋MAX,' + data['shopping_datetime'] + ',' + data['name'] + ',' + data['author'] + \
-                        str(data['price']) + ',' + str(data['buy_num']) + ',' + create_dt)
+            csv_list.append('通販本屋MAX,' + data['shopping_datetime'] + ',' + data['name'] + ',' + data['author'] + ',' \
+                        + str(data['price']) + ',' + str(data['buy_num']) + ',' + record_create_ts)
 
         with open('.\\files\\z_output_csv\\shopping_data_{0}.csv'.format(file_create_dt), mode='a', encoding='UTF-8') as wf:
             wf.write('\n'.join(csv_list))
+        
+        if flg:
+            logger.info('shopping_data_{0}.csv'.format(file_create_dt) + 'を新規作成しました。')
+        else:
+            logger.info('shopping_data_{0}.csv'.format(file_create_dt) + 'に追記しました。')
     except:
         logger.error('エラー')
         raise
@@ -32,7 +44,9 @@ def main():
         for f_path in glob.glob('.\\files\\json_ptn1\\*'):
             with open(f_path, mode='r', encoding='UTF-8') as f:
                 h.extend(json.load(f))
-        
+
+        logger.info("JSON読み込み正常終了。CSVファイルを作成します。")
+
         # CSVファイル作成
         create_csv(h)
 
@@ -44,8 +58,9 @@ def main():
 
 
 if __name__ == "__main__":
-    if main() == -1:
-        logger.info('～～正常終了～～')
+    logger.info('～～ フォーマットパターン1 処理開始 ～～')
+    if main() == 0:
+        logger.info('～～ 正常終了 ～～')
     else:
-        logger.error('～～異常終了～～')
+        logger.error('～～ 異常終了 ～～')
 
